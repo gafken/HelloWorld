62ea97032654dfd9a4e85e995eeca523c1cc8459
gafken <rtd91@live.com>
2015-07-06 14:58:59 -0400

    Update to Increase Features
    Added one feature to make a total of one features with update patch
    0.1.0.1.  Features include stating 'Hello', name retrieval, and saying
    "Hello" again and including the name that was retrieved onto the
    secondary prompt.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello.  What is your first name");  //prompt user to enter first name
+            string firstName = Console.ReadLine();  //capture first name
+            Console.WriteLine("Hello " + firstName);  //return "hello " and users first name
+        }
+
+        //function to call to for first/last name printout
+
+        static void displayName(bool libraryStyle, string lastName, string FirstName)
+        {
+            //if you want library style.  ie.  Doe, John.
+            if (libraryStyle)
+                Console.WriteLine(lastName + ", " + FirstName);
+            //if you don't want library style. ie. John Doe.
+            else
+                Console.WriteLine(FirstName + " " + lastName);
         }
     }
 }
