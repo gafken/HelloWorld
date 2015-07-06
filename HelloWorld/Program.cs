9038b49049663fe6cc784666e7d4e13493b53cb6
gafken <rtd91@live.com>
2015-07-06 16:16:19 -0400

    Update upon further instruction
    Upon more elaborate instruction, corrections have been made to include
    more features, such as name retrieval and proper use of the displayName
    function allowing for further formatting options for the user.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,21 +10,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello.  What is your first name");  //prompt user to enter first name
-            string firstName = Console.ReadLine();  //capture first name
-            Console.WriteLine("Hello " + firstName);  //return "hello " and users first name
+            String response1;       //variable to retrieve string sent from console response
+            int parsedResp = 0;     //stores the value returned after parsing the above srting
+            Console.WriteLine("What is your first name?");
+            String firstName = Console.ReadLine();      //Retrieve first name from user
+            Console.WriteLine();
+            Console.WriteLine("What is your last name?");
+            String lastName = Console.ReadLine();       //Retrieve last name from user
+            Console.WriteLine();
+            do{
+                Console.WriteLine("\nHow would you like me to address you?");
+                Console.WriteLine("1:  John Doe");
+                Console.WriteLine("2:  Doe, John");             //User chooses the format in which they wish to be addressed.
+                Console.WriteLine();
+                Console.WriteLine("Please enter 1 or 2.");
+                Console.WriteLine();
+
+                response1 = Console.ReadLine();                 //Retrieve reponse
+                Int32.TryParse(response1, out parsedResp);      //Parse response and save
+                if (parsedResp != 1 && parsedResp != 2)         //Test if parsed response is acceptable to continue
+                {
+                    Console.WriteLine();                        //if parsed response fails, this is purely for asthetics
+                    Console.WriteLine("Invalid input, please try again");
+                    Console.WriteLine();
+                }
+             }while (parsedResp != 1 && parsedResp != 2);
+            Console.Write("Hello ");                            //<--¬
+            displayName(parsedResp, lastName, firstName);       //Display Name in proper format
         }
 
         //function to call to for first/last name printout
 
-        static void displayName(bool libraryStyle, string lastName, string FirstName)
+        static void displayName(int libraryStyle, String lName, String fName)
         {
             //if you want library style.  ie.  Doe, John.
-            if (libraryStyle)
-                Console.WriteLine(lastName + ", " + FirstName);
+            if (libraryStyle == 2)
+                Console.WriteLine(lName + ", " + fName);
             //if you don't want library style. ie. John Doe.
             else
-                Console.WriteLine(FirstName + " " + lastName);
+                Console.WriteLine(fName + " " + lName);
         }
     }
 }
